c493998f7152c03e370fde87abe515dc7b1c29fa
Akash Pallath <pallathakash@gmail.com>
2020-03-02 14:57:01 -0500

    Update indus_waters.py

--- analysis/indus_waters.py
+++ analysis/indus_waters.py
@@ -2,7 +2,7 @@
 
 @Author: Akash Pallath
 """
-from . import timeseries
+import timeseries
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -37,24 +37,45 @@ class IndusWaters(timeseries.TimeSeries):
             self.t = np.array(t)
             self.N = np.array(N)
             self.Ntw = np.array(Ntw)
-            self.mu = mu
+            self.mu = np.float(mu)
 
     def __call__(self):
         self.get_data()
         #log data
         self.save_timeseries(self.t,self.N,label="N")
         self.save_timeseries(self.t,self.N,label="Ntw")
-        #plotting
+
+        #plot time series data
         fig, ax = plt.subplots()
         ax.plot(self.t,self.N,label="$N$")
-        ax.plot(self.t,self.Ntw,label="$Ntwiddle$")
+        ax.plot(self.t,self.Ntw,label="$N_{tw}$")
         ax.set_xlabel("Time, in ps")
         ax.set_ylabel("$N_v$ and coarse-grained $N_v$")
         ax.legend()
         self.save_figure(fig,suffix="waters")
         if self.args.show:
-            print("Displaying figure")
             plt.show()
+
+        #plot moving average data
+        maN = self.moving_average(self.N)
+        maNtw = self.moving_average(self.Ntw)
+        fig, ax = plt.subplots()
+        ax.plot(self.t, maN, label="$N$, moving average")
+        ax.plot(self.t, maNtw, label="$N_{tw}$, moving average")
+        ax.set_xlabel("Time, in ps")
+        ax.set_ylabel("Number of waters, moving average")
+        ax.legend()
+        self.save_figure(fig,suffix="ma_waters")
+        if self.args.show:
+            plt.show()
+
+        #getaverages
+        N_eff_samp, N_mean, N_std, N_sem, N_tau = self.average(self.t, self.N)
+        Ntw_eff_samp, Ntw_mean, Ntw_std, Ntw_sem, Ntw_tau = self.average(self.t, self.Ntw)
+        #append averages
+        if self.args.avgto is not None:
+            with open(self.args.avgto, "a+") as f:
+                f.write("{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\n".format(self.mu, N_eff_samp, N_mean, N_std, N_sem, Ntw_eff_samp, Ntw_mean, Ntw_std, Ntw_sem, N_tau, Ntw_tau))
 
 if __name__=="__main__":
     waters = IndusWaters()
