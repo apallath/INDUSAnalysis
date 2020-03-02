7bcd8e04c56a5162a479c521c43e59dba6d5ad44
Akash Pallath <pallathakash@gmail.com>
2020-03-02 14:57:30 -0500

    Update protein_order_params.py

--- analysis/protein_order_params.py
+++ analysis/protein_order_params.py
@@ -1,71 +1,82 @@
-"""Calculate time series
-- Radius of gyration
-- RMSD
-given GROMACS trajectory of a protein
+"""Plot number of waters in probe volume output by GROMACS-INDUS simulation
 
 @Author: Akash Pallath
 """
-from . import timeseries
+import timeseries
 
 import numpy as np
 import matplotlib.pyplot as plt
-import MDAnalysis as mda
-import MDAnalysis.analysis.rms as mda_rms
 
-class OrderParams(timeseries.TimeSeries):
+class IndusWaters(timeseries.TimeSeries):
     def __init__(self):
         super().__init__()
-        self.parser.add_argument("structf", help="Structure file (.gro)")
-        self.parser.add_argument("trajf", help="Compressed trajectory file (.xtc)")
-        self.parser.add_argument("-select", help="atoms/groups to track order parameters for (MDA selection string)")
-        self.parser.add_argument("-align", help="atoms/groups to align to reference frame (MDA selection string)")
+        self.parser.add_argument("file", help="GROMACS-INDUS waters data file")
 
-    def calc_Rg(self,universe,selection):
-        Rg = []
-        sel = universe.select_atoms(selection)
-        for ts in universe.trajectory:
-            Rg.append((universe.trajectory.time, sel.radius_of_gyration()))
-        Rg = np.array(Rg)
-        return Rg
+    def get_data(self):
+        self.read_args()
+        t = []
+        N = []
+        Ntw = []
+        mu = 0
+        with open(self.args.file) as f:
+            #read data file
+            for l in f:
+                lstrip = l.strip()
+                #parse comments
+                if lstrip[0]=='#':
+                    comment = lstrip[1:].split()
+                    if comment[0] == 'mu':
+                        mu = comment[2]
+                #parse data
+                if lstrip[0]!='#':
+                    (tcur,Ncur,Ntwcur) = map(float,lstrip.split())
+                    t.append(tcur)
+                    N.append(Ncur)
+                    Ntw.append(Ntwcur)
 
-    def calc_RMSD(self):
-        RMSD = []
-        sel = self.u.select_atoms(self.selection)
-        #initial positions
-        initpos = sel.positions.copy()
-        for ts in self.u.trajectory:
-            RMSD.append((self.u.trajectory.time, mda_rms.rmsd(initpos,sel.positions.copy(),superposition=True)))
-        RMSD = np.array(RMSD)
-        return RMSD
+            self.t = np.array(t)
+            self.N = np.array(N)
+            self.Ntw = np.array(Ntw)
+            self.mu = np.float(mu)
 
     def __call__(self):
-        #definition in TimeSeries base class
-        self.read_args()
-        self.selection = self.args.selection
-        if self.selection == None:
-            self.selection = 'protein'
-        self.u = mda.Universe(self.args.structf,self.args.trajf)
+        self.get_data()
+        #log data
+        self.save_timeseries(self.t,self.N,label="N")
+        self.save_timeseries(self.t,self.N,label="Ntw")
 
-        #radius of gyration
-        sel_rg = self.calc_Rg(self.u, self.selection)
-        #plot
+        #plot time series data
         fig, ax = plt.subplots()
-        ax.plot(sel_rg[:,0],sel_rg[:,1]);
-        ax.set_xlabel("Time (ps)")
-        ax.set_ylabel("Radius of gyration (nm)")
-        self.save_figure(fig,suffix="Rg")
-
-        #RMSD from initial structure
-        sel_RMSD = self.calc_RMSD()
-        #plot
-        fig, ax = plt.subplots()
-        ax.plot(sel_RMSD[:,0],sel_RMSD[:,1])
-        ax.set_xlabel("Time (ps)")
-        ax.set_ylabel("RMSD (nm)")
-        self.save_figure(fig,suffix="RMSD")
+        ax.plot(self.t,self.N,label="$N$")
+        ax.plot(self.t,self.Ntw,label="$N_{tw}$")
+        ax.set_xlabel("Time, in ps")
+        ax.set_ylabel("$N_v$ and coarse-grained $N_v$")
+        ax.legend()
+        self.save_figure(fig,suffix="waters")
         if self.args.show:
             plt.show()
 
+        #plot moving average data
+        maN = self.moving_average(self.N)
+        maNtw = self.moving_average(self.Ntw)
+        fig, ax = plt.subplots()
+        ax.plot(self.t, maN, label="$N$, moving average")
+        ax.plot(self.t, maNtw, label="$N_{tw}$, moving average")
+        ax.set_xlabel("Time, in ps")
+        ax.set_ylabel("Number of waters, moving average")
+        ax.legend()
+        self.save_figure(fig,suffix="ma_waters")
+        if self.args.show:
+            plt.show()
+
+        #getaverages
+        N_eff_samp, N_mean, N_std, N_sem, N_tau = self.average(self.t, self.N)
+        Ntw_eff_samp, Ntw_mean, Ntw_std, Ntw_sem, Ntw_tau = self.average(self.t, self.Ntw)
+        #append averages
+        if self.args.avgto is not None:
+            with open(self.args.avgto, "a+") as f:
+                f.write("{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\n".format(self.mu, N_eff_samp, N_mean, N_std, N_sem, Ntw_eff_samp, Ntw_mean, Ntw_std, Ntw_sem, N_tau, Ntw_tau))
+
 if __name__=="__main__":
-    prot = OrderParams()
-    prot()
+    waters = IndusWaters()
+    waters()
