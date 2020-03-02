5ea1aac43848dedb6339403df0dc817b1ee805db
Akash Pallath <pallathakash@gmail.com>
2020-03-02 14:58:11 -0500

    Update timeseries.py

--- analysis/timeseries.py
+++ analysis/timeseries.py
@@ -19,6 +19,7 @@ class TimeSeries:
         self.parser.add_argument("-avgstart", help="time to start averaging at")
         self.parser.add_argument("-avgend", help="time to stop averaging at")
         self.parser.add_argument("-avgto", help="file to append averages to")
+        self.parser.add_argument("-window", help="window for moving average (default = 10)")
         #plotting
         self.parser.add_argument("-opref", help="output image and data prefix")
         self.parser.add_argument("-oformat", help="output image format")
@@ -30,37 +31,48 @@ class TimeSeries:
 
     def save_timeseries(self,t,x,label=""):
         ts = np.stack((t,x))
-        np.save(self.args.opref+"_"+label,ts)
+        pref = self.args.opref
+        if pref == None:
+            pref = "data"
+        np.save(pref+"_"+label,ts)
 
-    def moving_average(self,t,x,window):
+    def moving_average(self,x):
+        window = self.args.window
+        if window is None:
+            window = np.float(10.0)
+        else:
+            window = np.float(window)
         w = np.repeat(1.0, window)/window
         ma = np.convolve(x, w, 'same')
-        return t, ma
+        return ma
 
     def average(self,t,x):
         tstep = t[1] - t[0]
         start = 0
         end = len(x) - 1
-        if self.avgstart is not None:
-            start = int(floor(self.avgstart/tstep))
-        if self.avgend is not None:
-            end = int(floor(self.avgend/tstep))
+        if self.args.avgstart is not None:
+            start = int(np.floor(np.float(self.args.avgstart)/tstep))
+        if self.args.avgend is not None:
+            end = int(np.floor(np.float(self.args.avgend)/tstep))
         te = t[start:end]
         xe = x[start:end]
         tau_step = pymbar.timeseries.integratedAutocorrelationTime(xe)
         Neff = len(xe)/tau_step
 
         #Calculate std and sem
+        mean = 0
         std = 0
         sem = 0
         for i in range(1000):
-            sidx = np.random.choice(np.array(range(len(xe))), size = int(N_uncorr), replace=False)
+            #sample without replacement
+            sidx = np.random.choice(np.array(range(len(xe))), size = int(Neff), replace=False)
+            mean += np.mean(xe[sidx])
             std += np.std(xe[sidx])
             sem += stats.sem(xe[sidx])
+        mean/= 1000
         std/= 1000
         sem/= 1000
-        return std, sem
-
+        return Neff, mean, std, sem, tau_step
 
     def save_figure(self,fig,suffix=""):
         oformat = self.args.oformat
